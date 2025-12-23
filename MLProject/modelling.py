aa4c2f6f96121e517658f15f45cd55d5a47cad25
Jea <146644799+jeaconia@users.noreply.github.com>
2025-12-24 01:40:50 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,41 +1,42 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-#Load Data
-X_train = pd.read_csv("Abalone_preprocessing/X_train.csv")
-X_test = pd.read_csv("Abalone_preprocessing/X_test.csv")
-y_train = pd.read_csv("Abalone_preprocessing/y_train.csv").values.ravel()
-y_test = pd.read_csv("Abalone_preprocessing/y_test.csv").values.ravel()
 
-#Set Eksperimen
-mlflow.set_tracking_uri("file:mlruns")
-mlflow.set_experiment("Eksperimen Latih Model Dataset Abalone")
-
-with mlflow.start_run() as run:
-    #Training Model
-    model = RandomForestClassifier(
-        n_estimators=100,
-        random_state=42
-    )
-        
-    model.fit(X_train, y_train)
-    
-    #Evaluasi
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    
-    print("Accuracy:", accuracy)
-    print(classification_report(y_test, y_pred))
-
-    mlflow.log_metric("accuracy", accuracy)
-    mlflow.sklearn.log_model(model, artifact_path="model")
+DATA_DIR = "Abalone_preprocessing"
 
 
+def main():
+    mlflow.autolog()
 
+    with mlflow.start_run() as run:
+        print(f"MLFLOW_RUN_ID={run.info.run_id}")
 
+        # Load data
+        X_train = pd.read_csv(f"{DATA_DIR}/X_train.csv")
+        X_test = pd.read_csv(f"{DATA_DIR}/X_test.csv")
+        y_train = pd.read_csv(f"{DATA_DIR}/y_train.csv").values.ravel()
+        y_test = pd.read_csv(f"{DATA_DIR}/y_test.csv").values.ravel()
 
+        # Model
+        model = RandomForestClassifier(
+            n_estimators=100,
+            random_state=42
+        )
 
+        model.fit(X_train, y_train)
 
+        # Evaluation
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+
+        print("Accuracy:", accuracy)
+        print(classification_report(y_test, y_pred))
+
+        mlflow.log_metric("accuracy_manual", accuracy)
+
+if __name__ == "__main__":
+    main()
