3c0a6486b86975a5c0585b1228c999c6c468483e
Jea <146644799+jeaconia@users.noreply.github.com>
2025-12-24 07:06:50 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -9,39 +9,28 @@ DATA_DIR = "Abalone_preprocessing"
 
 
 def main():
-    mlflow.set_experiment("abalone-experiment")
+    X_train = pd.read_csv(f"{DATA_DIR}/X_train.csv")
+    X_test = pd.read_csv(f"{DATA_DIR}/X_test.csv")
+    y_train = pd.read_csv(f"{DATA_DIR}/y_train.csv").values.ravel()
+    y_test = pd.read_csv(f"{DATA_DIR}/y_test.csv").values.ravel()
 
-        print(f"MLFLOW_RUN_ID={run.info.run_id}")
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
 
-        # Load data
-        X_train = pd.read_csv(f"{DATA_DIR}/X_train.csv")
-        X_test = pd.read_csv(f"{DATA_DIR}/X_test.csv")
-        y_train = pd.read_csv(f"{DATA_DIR}/y_train.csv").values.ravel()
-        y_test = pd.read_csv(f"{DATA_DIR}/y_test.csv").values.ravel()
+    mlflow.sklearn.log_model(
+        model,
+        artifact_path="model"
+    )
 
-        # Model
-        model = RandomForestClassifier(
-            n_estimators=100,
-            random_state=42
-        )
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+    mlflow.log_metric("accuracy", acc)
 
-        model.fit(X_train, y_train)
-
-        mlflow.sklearn.log_model(
-            sk_model=model,
-            artifact_path="model",
-            registered_model_name=None
-        )
-
-        # Evaluation
-        preds = model.predict(X_test)
-        acc = accuracy_score(y_test, preds)
-
-        mlflow.log_metric("accuracy", acc)
-
-        print(f"Training selesai | ACC={acc:.4f}")
+    print("Training & logging selesai")
 
 
 if __name__ == "__main__":
     main()
-
